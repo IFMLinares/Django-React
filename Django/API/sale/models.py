517a6b2c2cae22cdb5dc57a6472656a8f142b409
Miguel Linares <ifmlinares@gmail.com>
2025-09-08 01:07:09 -0400

    feat: Add validation to ensure seller is the owner of the business in Sale model

--- Django/API/sale/models.py
+++ Django/API/sale/models.py
@@ -1,5 +1,5 @@
-
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from API.products.models import Product, ProductVariant, Business
 
@@ -26,6 +26,13 @@ class Sale(models.Model):
     notes = models.TextField(blank=True, null=True)
     whatsapp_message = models.TextField(blank=True, null=True)
     whatsapp_number = models.CharField(max_length=20, blank=True, null=True)
+
+    def clean(self):
+        if self.business.user != self.seller:
+            raise ValidationError("El vendedor debe ser el propietario del negocio.")
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-sale_date']
