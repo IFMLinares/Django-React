a71ea5926fd68fec106000fb614f08f9ce92a748
Miguel Linares <ifmlinares@gmail.com>
2026-02-18 23:50:28 -0400

    feat: Update .gitignore to include .github directory and add new CORS origins in settings.py

--- Django/settings/settings.py
+++ Django/settings/settings.py
@@ -138,7 +138,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
 ]
+
 
 
 CORS_ALLOW_CREDENTIALS = True
